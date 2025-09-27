bb9611c13338fc2f16b7b911623e44ab6210354a
lucruz1984-ti <lucruz1984@gmail.com>
2025-09-27 00:54:40 +0000

    Criei as Cartas do jogo Super Trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,81 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+char carta;
+char estado;
+char codigo[4];
+char cidade [20];
+int populacao;
+float area;
+float PIB;
+int pontosturisticos;
 
   // Área para entrada de dados
+printf("Carta 1: \n", carta);
+
+printf("Digite o estado (uma letra de A a H): \n");
+scanf("%c", &estado);
+
+printf("Digite o código da carta (letra seguida de 2 números) \n");
+scanf("%s", codigo);
+
+printf("Digite o nome da cidade (sem espaços): \n");
+scanf("%s", cidade);
+
+printf("Digite a população da cidade: \n");
+scanf("%d", &populacao);
+
+printf("Digite a área da cidade (em km²): \n");
+scanf("%f", &area);
+
+printf("Digite o PIB da cidade (em bilhões de reais): \n");
+scanf("%f", &PIB);
+
+printf("Digite o número de pontos turísticos da cidade: \n");
+scanf("%d", &pontosturisticos);
+
+printf("Carta 2: \n", carta);
+printf("Digite o estado (uma letra de A a H): \n");
+scanf("%c", &estado);
+
+printf("Digite o código da carta (letra seguida de 2 números): \n");
+scanf("%s", codigo);
+
+printf("Digite o nome da cidade (sem espaços): \n");
+scanf("%s", cidade);
+
+printf("Digite a população da cidade: \n");
+scanf("%d", &populacao);
+
+printf("Digite a área da cidade (em km²): \n");
+scanf("%f", &area);
+
+printf("Digite o PIB da cidade (em bilhões de reais): \n");
+scanf("%f", &PIB);
+
+printf("Digite o número de pontos turísticos da cidade: \n");
+scanf("%d", &pontosturisticos);
 
   // Área para exibição dos dados da cidade
+printf("Carta 1: \n", carta);
+printf("Estado: %c\n", estado);
+printf("Código: %s\n", codigo);
+printf("Nome da Cidade: %s\n", cidade);
+printf("População: %d\n", populacao);
+printf("Área: %.2f km²\n", area);
+printf("PIB: %.2f bilhões de reais\n", PIB);
+print("Número de Pontos Turísticos: %d\n", pontosturisticos);
+
+printf("\n");
+
+printf("Carta 2: \n", carta);
+printf("Estado: %c\n", estado);
+printf("Código: %s\n", codigo);
+printf("Nome da Cidade: %s\n", cidade);
+printf("População: %d\n", populacao);
+printf("Área: %.2f km²\n", area);
+printf("PIB: %.2f bilhões de reais\n", PIB);
+printf("Número de Pontos Turísticos: %d\n", pontosturisticos);
 
 return 0;
 } 
